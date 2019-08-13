d75df4ba8bf68af6ab81961341212540f8bf3f84
nhjjreuter <niels.reuter@gmail.com>
2019-08-13 11:41:10 +0200

    cleanup

--- build/lib/cbptools/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""CBP tools is a NeuroImaging tool for Connectivity-Based Parcellation"""
-from . import clean, cluster, connectivity, exceptions, image, utils
-from ._version import __version__
-
-__readthedocs__ = 'http://docs.inm7.de'
-__all__ = ['clean', 'cluster', 'connectivity', 'exceptions', 'image', 'utils',
-           '__version__', '__readthedocs__']
